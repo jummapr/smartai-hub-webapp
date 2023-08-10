55c57910657a93974bd908cef3c24442f77cd68c
jumma <aarifhingorja4@gmail.com>
2023-08-10 15:25:11 +0530

    finshed chat api

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -29,8 +29,7 @@ export async function POST(
 
     const companion = await prismadb.companion.update({
       where: {
-        id: params.chatId,
-        userId: user.id,
+        id: params.chatId
       },
       data: {
         messages: {
@@ -40,53 +39,108 @@ export async function POST(
             userId: user.id,
           },
         },
-      },
+      }
     });
 
-    if(!companion) {
-        return new NextResponse("Companion not fund", {status: 404})
-    };
+    if (!companion) {
+      return new NextResponse("Companion not found", { status: 404 });
+    }
 
     const name = companion.id;
     const companion_file_name = name + ".txt";
 
     const companionKey = {
-        companionName: name,
-        userId: user.id,
-        modelName: "llama2-13b",
+      companionName: name!,
+      userId: user.id,
+      modelName: "llama2-13b",
     };
-
     const memoryManager = await MemoryManager.getInstance();
 
     const records = await memoryManager.readLatestHistory(companionKey);
-
-    if(records.length === 0) {
-        await memoryManager.seedChatHistory(companion.seed, "\n\n",companionKey);
+    if (records.length === 0) {
+      await memoryManager.seedChatHistory(companion.seed, "\n\n", companionKey);
     }
+    await memoryManager.writeToHistory("User: " + prompt + "\n", companionKey);
 
-    await memoryManager.writeToHistory("User: " + prompt + "\n", companionKey)
+    // Query Pinecone
 
     const recentChatHistory = await memoryManager.readLatestHistory(companionKey);
 
+    // Right now the preamble is included in the similarity search, but that
+    // shouldn't be an issue
+
     const similarDocs = await memoryManager.vectorSearch(
       recentChatHistory,
-      companion_file_name,
+      companion_file_name
     );
 
     let relevantHistory = "";
+    if (!!similarDocs && similarDocs.length !== 0) {
+      relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
+    }
+    const { handlers } = LangChainStream();
+    // Call Replicate for inference
+    const model = new Replicate({
+      model:
+        "a16z-infra/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+      input: {
+        max_length: 2048,
+      },
+      apiKey: process.env.REPLICATE_API_TOKEN,
+      callbackManager: CallbackManager.fromHandlers(handlers),
+    });
 
-      if(!!similarDocs && similarDocs.length !== 0) {
-        relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
-      };
+    // Turn verbose on for debugging
+    model.verbose = true;
 
-      const {handlers} = LangChainStream();
+    const resp = String(
+      await model
+        .call(
+          `
+        ONLY generate plain sentences without prefix of who is speaking. DO NOT use ${companion.name}: prefix. 
 
-      // const model = new Replicate({
-      //   model: 
-      // })
+        ${companion.instructions}
 
+        Below are relevant details about ${companion.name}'s past and the conversation you are in.
+        ${relevantHistory}
+
+
+        ${recentChatHistory}\n${companion.name}:`
+        )
+        .catch(console.error)
+    );
+
+    const cleaned = resp.replaceAll(",", "");
+    const chunks = cleaned.split("\n");
+    const response = chunks[0];
+
+    await memoryManager.writeToHistory("" + response.trim(), companionKey);
+    var Readable = require("stream").Readable;
+
+    let s = new Readable();
+    s.push(response);
+    s.push(null);
+    if (response !== undefined && response.length > 1) {
+      memoryManager.writeToHistory("" + response.trim(), companionKey);
+
+      await prismadb.companion.update({
+        where: {
+          id: params.chatId
+        },
+        data: {
+          messages: {
+            create: {
+              content: response.trim(),
+              role: "system",
+              userId: user.id,
+            },
+          },
+        }
+      });
+    }
+
+    return new StreamingTextResponse(s);
   } catch (error) {
-    console.log("[CHAT_POST]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
-}
+};
